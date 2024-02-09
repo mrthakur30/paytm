fee39d844f9e24017c44a4fa702595fae326d9c3
Mukul Thakur <95705739+mrthakur30@users.noreply.github.com>
2024-02-09 11:11:45 +0530

    cors middlware

--- backend/index.js
+++ backend/index.js
@@ -5,8 +5,13 @@ const app = express()
 const rootRouter = require("./routes/root");
 require('dotenv').config()
 
-app.use(cors({origin : 'https://main--frolicking-dolphin-f0f06c.netlify.app'}))
+app.use(cors())
 const PORT = process.env.PORT || 8080 ;
+
+app.use((req, res, next) => {
+   res.header("Access-Control-Allow-Origin", "*");
+   next();
+});
 
 app.use(bodyParser.json());
 app.use(express.json());

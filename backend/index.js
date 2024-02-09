48fe51038568f73fa8e12253cfe96e03c40897a5
Mukul Thakur <95705739+mrthakur30@users.noreply.github.com>
2024-02-09 11:49:24 +0530

    hoja

--- backend/index.js
+++ backend/index.js
@@ -8,12 +8,6 @@ require('dotenv').config()
 app.use(cors())
 const PORT = process.env.PORT || 8080;
 
-app.all('*', function (req, res) {
-   res.header("Access-Control-Allow-Origin", "*");
-   res.header("Access-Control-Allow-Headers", "Content-Type,Content-Length, Authorization, Accept,X-Requested-With");
-   res.header("Access-Control-Allow-Methods", "PUT,POST,GET,DELETE,OPTIONS");
-});
-
 app.use(bodyParser.json());
 app.use(express.json());
 

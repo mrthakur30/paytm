161e5184fa7db0d53227f8206c4a272d5ebcba47
Mukul Thakur <95705739+mrthakur30@users.noreply.github.com>
2024-02-09 11:31:16 +0530

    cors-pleses-get-solved

--- backend/index.js
+++ backend/index.js
@@ -6,18 +6,22 @@ const rootRouter = require("./routes/root");
 require('dotenv').config()
 
 app.use(cors())
-const PORT = process.env.PORT || 8080 ;
+const PORT = process.env.PORT || 8080;
 
-app.use((req, res, next) => {
+app.all('*', function (req, res) {
    res.header("Access-Control-Allow-Origin", "*");
-   next();
+   res.header("Access-Control-Allow-Headers", "Content-Type,Content-Length, Authorization, Accept,X-Requested-With");
+   res.header("Access-Control-Allow-Methods", "PUT,POST,GET,DELETE,OPTIONS");
 });
 
+app.use();
 app.use(bodyParser.json());
 app.use(express.json());
 
-app.use("/api/v1",rootRouter);
 
-app.listen(PORT,()=>{
-   console.log("Listening on port",PORT);
+
+app.use("/api/v1", , rootRouter);
+
+app.listen(PORT, () => {
+   console.log("Listening on port", PORT);
 })
